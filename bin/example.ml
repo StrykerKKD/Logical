c051806320ad8f7d56446e21b0dd90fc7561a22b
Robert Toth <kkdstryker@gmail.com>
2019-03-08 23:37:17 +0100

    Removing pursue_goal.

--- bin/example.ml
+++ bin/example.ml
@@ -2,7 +2,7 @@ open Logical
 
 let my_set = Base.Set.of_list (module ValueComparator) [ValueType.Int 1; ValueType.Int 2; ValueType.Int 3]
 let example_goal = in_set_goal (ValueType.Var "a") (ValueType.Set my_set)
-let result = pursue_goal example_goal empty_state
+let result = example_goal empty_state
 let result_list = Base.Sequence.to_list_rev result
 let () = Base.List.iter result_list ~f:(fun element ->
   match element with
--- lib/logical.ml
+++ lib/logical.ml
@@ -80,44 +80,41 @@ let unify_state state value_a value_b =
   | value, ValueType.Var variable -> Some (add_assignment state (variable, value))
   | _, _ -> None
 
-let pursue_goal goal state =
-  goal state
-
 let equal_goal value_a value_b = (fun state -> 
   let state = unify_state state value_a value_b in
   Base.Sequence.singleton state
 )
 
 let either_goal first_goal second_goal = (fun state -> 
-  let first_stream = pursue_goal first_goal state in
-  let second_stream = pursue_goal second_goal state in
+  let first_stream = first_goal state in
+  let second_stream = second_goal state in
   Base.Sequence.round_robin [first_stream; second_stream]
 )
 
 let either_goal_general goals = (fun state -> 
-  let streams = Base.List.map goals ~f:(fun goal -> pursue_goal goal state) in
+  let streams = Base.List.map goals ~f:(fun goal -> goal state) in
   Base.Sequence.round_robin streams
 )
 
 let both_goal first_goal second_goal = (fun state ->
-  let first_states = pursue_goal first_goal state in
+  let first_states = first_goal state in
   let sum_states = Base.Sequence.filter_map first_states ~f:(fun first_state -> 
     match first_state with
-    | Some state -> Some (pursue_goal second_goal state)
+    | Some state -> Some (second_goal state)
     | None -> None
   ) in
   Base.Sequence.interleave sum_states
 )
 
-let desugar_set_to_logic set variable state =
+let pursue_desugared_set set variable state =
   let value_list = Base.Set.to_list set in
   let both_goals = Base.List.map value_list ~f:(fun value -> equal_goal (ValueType.Var variable) value) in
   let either_goal = either_goal_general both_goals in 
-  pursue_goal either_goal state
+  either_goal state
 
 let in_set_goal value_a value_b = (fun state -> 
   match value_a, value_b with
-  | ValueType.Var variable, ValueType.Set set -> desugar_set_to_logic set variable state
-  | ValueType.Set set, ValueType.Var variable -> desugar_set_to_logic set variable state
+  | ValueType.Var variable, ValueType.Set set -> pursue_desugared_set set variable state
+  | ValueType.Set set, ValueType.Var variable -> pursue_desugared_set set variable state
   | _ -> Base.Sequence.singleton None
 )
