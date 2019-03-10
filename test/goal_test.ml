66a43beecb59dc9e313193ed32e54fd618d774db
Robert Toth <kkdstryker@gmail.com>
2019-03-10 13:33:26 +0100

    Covering goal with unit tests.

---
+++ test/goal_test.ml
@@ -0,0 +1,70 @@
+open Logical
+open Goal
+
+let test_equal_on_value_variable_and_empty_state () =
+  let expected = [Some ["a",Value.int 1]] in
+  let actual = equal (Value.var "a") (Value.int 1) State.empty |> Base.Sequence.to_list in
+  Alcotest.(check int) "equal should return with state which contains the value variable" 
+    0 (compare expected actual)
+
+let test_equal_on_value_variable_and_matching_state () =
+  let expected = [Some ["a",Value.int 1;"b",Value.int 1]] in
+  let actual = equal (Value.var "a") (Value.var "b") ["b",Value.int 1] |> Base.Sequence.to_list in
+  Alcotest.(check int) "equal should return with state which contains the value variable" 
+    0 (compare expected actual)
+
+let test_equal_on_set_variable_and_empty_state () =
+  let test_set = Value.set (Base.Set.singleton (module Value.Comparator) (Value.int 1)) in
+  let expected = [Some ["a",test_set]] in
+  let actual = equal (Value.var "a") test_set State.empty |> Base.Sequence.to_list in
+  Alcotest.(check int) "equal should return with state which contains the set variable" 
+    0 (compare expected actual)
+
+let test_equal_on_set_variable_and_matching_state () =
+  let test_set = Value.set (Base.Set.singleton (module Value.Comparator) (Value.int 1)) in
+  let expected = [Some ["a",test_set;"b",test_set]] in
+  let actual = equal (Value.var "a") (Value.var "b") ["b",test_set] |> Base.Sequence.to_list in
+  Alcotest.(check int) "equal should return with state which contains the set variable" 
+    0 (compare expected actual)
+
+let test_either_on_variables_and_empty_state () =
+  let expected = [Some["a",Value.int 1]; Some["b",Value.int 2]] in
+  let actual = either 
+    (equal (Value.var "a") (Value.int 1)) (equal (Value.var "b") (Value.int 2)) State.empty
+    |> Base.Sequence.to_list in
+  Alcotest.(check int) "either should return goal results in different states" 
+    0 (compare expected actual)
+
+let test_either_multi_on_variables_and_empty_state () =
+  let expected = [Some["a",Value.int 1]; Some["b",Value.int 2]] in
+  let actual = either_multi
+    [equal (Value.var "a") (Value.int 1); equal (Value.var "b") (Value.int 2)] State.empty
+    |> Base.Sequence.to_list in
+  Alcotest.(check int) "either_multi should return goal results in different states" 
+    0 (compare expected actual)
+
+let test_both_on_variables_and_empty_state () =
+  let expected = [Some["b",Value.int 2; "a",Value.int 1]] in
+  let actual = both 
+    (equal (Value.var "a") (Value.int 1)) (equal (Value.var "b") (Value.int 2)) State.empty
+    |> Base.Sequence.to_list in
+  Alcotest.(check int) "both should return goal results in one state" 
+    0 (compare expected actual)
+
+let test_in_set_on_variables_and_empty_state () =
+  let test_set = Value.set (Base.Set.of_list (module Value.Comparator) [Value.int 1; Value.int 2]) in
+  let expected = [Some["a",Value.int 1]; Some["a",Value.int 2]] in
+  let actual = in_set (Value.var "a") test_set State.empty |> Base.Sequence.to_list in
+  Alcotest.(check int) "in_set should return goal result like either, but with only one variable" 
+    0 (compare expected actual)
+
+let tests = [
+  "test_equal_on_value_variable_and_empty_state", `Quick, test_equal_on_value_variable_and_empty_state;
+  "test_equal_on_value_variable_and_matching_state", `Quick, test_equal_on_value_variable_and_matching_state;
+  "test_equal_on_set_variable_and_empty_state", `Quick, test_equal_on_set_variable_and_empty_state;
+  "test_equal_on_set_variable_and_matching_state", `Quick, test_equal_on_set_variable_and_matching_state;
+  "test_either_on_variables_and_empty_state", `Quick, test_either_on_variables_and_empty_state;
+  "test_either_multi_on_variables_and_empty_state", `Quick, test_either_multi_on_variables_and_empty_state;
+  "test_both_on_variables_and_empty_state", `Quick, test_both_on_variables_and_empty_state;
+  "test_in_set_on_variables_and_empty_state", `Quick, test_in_set_on_variables_and_empty_state;
+]
--- test/run_tests.ml
+++ test/run_tests.ml
@@ -1,6 +1,7 @@
 let test_suites = [
-    "Value_test", Value_test.tests;
-    "State_test", State_test.tests;
+  "Value_test", Value_test.tests;
+  "State_test", State_test.tests;
+  "Goal_test", Goal_test.tests;
 ]
 
 let () = Alcotest.run "logical" test_suites
