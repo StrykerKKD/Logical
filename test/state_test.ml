ad178c9113361a083738ffbb0e5169f7fde6a316
Robert Toth <kkdstryker@gmail.com>
2019-03-10 12:21:38 +0100

    Formatting tests.

--- test/state_test.ml
+++ test/state_test.ml
@@ -6,64 +6,59 @@ let test_empty_state () =
 
 let test_to_string_on_empty_state () = 
   Alcotest.(check string) "to_string should return empty string for empty state" 
-  "" (to_string empty)
+    "" (to_string empty)
 
 let test_to_string_on_non_empty_state () =
   Alcotest.(check string) "to_string should return specified value for nonempty state" 
-  "(Assignments((a(Int 1))(b(Int 2))))" (to_string ["a",Value.int 1;"b",Value.int 2])
+    "(Assignments((a(Int 1))(b(Int 2))))" (to_string ["a",Value.int 1;"b",Value.int 2])
 
 let test_value_of_on_non_variable () =
   let expected = Value.int 1 in
   let actual = value_of empty (Value.int 1) in
   Alcotest.(check bool) "value_of should return value directly in case of value input"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_value_of_on_non_matching_variable () =
   let expected = Value.var "a" in
   let actual = value_of ["b",Value.int 2] (Value.var "a") in
   Alcotest.(check bool) "value_of should return new variable in non matching case"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_value_of_on_matching_variable () =
   let expected = Value.int 1 in
   let actual = value_of ["a",Value.int 1] (Value.var "a") in
   Alcotest.(check bool) "value_of should return value in matching case"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_equal_variable_values () =
   let expected = Some ["a",Value.int 1; "b",Value.int 1] in
-  let actual = unify ["a",Value.int 1; "b",Value.int 1] 
-    (Value.var "a") (Value.var "b")in
+  let actual = unify ["a",Value.int 1; "b",Value.int 1] (Value.var "a") (Value.var "b") in
   Alcotest.(check bool) "unify should return the a state which is equal to the input state"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_new_left_variable () =
   let expected = Some ["a",Value.int 1; "b",Value.int 1] in
-  let actual = unify ["b",Value.int 1] 
-    (Value.var "a") (Value.var "b")in
+  let actual = unify ["b",Value.int 1] (Value.var "a") (Value.var "b") in
   Alcotest.(check bool) "unify should return a new state with added left variable"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_new_right_variable () =
   let expected = Some ["b",Value.int 1; "a",Value.int 1] in
-  let actual = unify ["a",Value.int 1] 
-    (Value.var "a") (Value.var "b")in
+  let actual = unify ["a",Value.int 1] (Value.var "a") (Value.var "b") in
   Alcotest.(check bool) "unify should return a new state with added right variable"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_non_equal_variable_values () =
   let expected = None in
-  let actual = unify ["a",Value.int 1; "b",Value.int 2] 
-    (Value.var "a") (Value.var "b")in
+  let actual = unify ["a",Value.int 1; "b",Value.int 2] (Value.var "a") (Value.var "b") in
   Alcotest.(check bool) "unify should return None when variable value are not the same"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_non_equal_values () =
   let expected = None in
-  let actual = unify ["a",Value.int 1; "b",Value.int 1] 
-    (Value.int 1) (Value.int 2)in
+  let actual = unify ["a",Value.int 1; "b",Value.int 1] (Value.int 1) (Value.int 2) in
   Alcotest.(check bool) "unify should return None when both input is non variable"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_two_equal_set () =
   let test_set_a = Value.set (Base.Set.of_list (module Value.Comparator) 
@@ -71,10 +66,9 @@ let test_unify_on_two_equal_set () =
   let test_set_b = Value.set (Base.Set.of_list (module Value.Comparator) 
     [Value.int 3; Value.int 2; Value.int 1]) in
   let expected = Some ["a",Value.int 1; "b",Value.int 1] in
-  let actual = unify ["a",Value.int 1; "b",Value.int 1] 
-    test_set_a test_set_b in
+  let actual = unify ["a",Value.int 1; "b",Value.int 1] test_set_a test_set_b in
   Alcotest.(check bool) "unify should return state when both set is equal"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_two_non_equal_set () =
   let test_set_a = Value.set (Base.Set.of_list (module Value.Comparator) 
@@ -82,19 +76,17 @@ let test_unify_on_two_non_equal_set () =
   let test_set_b = Value.set (Base.Set.of_list (module Value.Comparator) 
     [Value.int 4; Value.int 5; Value.int 6]) in
   let expected = None in
-  let actual = unify ["a",Value.int 1; "b",Value.int 1] 
-    test_set_a test_set_b in
+  let actual = unify ["a",Value.int 1; "b",Value.int 1] test_set_a test_set_b in
   Alcotest.(check bool) "unify should return None when both set is non equal"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_set_and_value () =
   let test_set = Value.set (Base.Set.of_list (module Value.Comparator) 
     [Value.int 1; Value.int 2; Value.int 3]) in
   let expected = None in
-  let actual = unify ["a",Value.int 1; "b",Value.int 1] 
-    test_set (Value.int 1) in
+  let actual = unify ["a",Value.int 1; "b",Value.int 1] test_set (Value.int 1) in
   Alcotest.(check bool) "unify should return none when input is a set and a value"
-  true (expected = actual)
+    true (expected = actual)
 
 let test_unify_on_set_and_variable () =
   let test_set = Value.set (Base.Set.of_list (module Value.Comparator) 
