31c1a19db0c61666ad027ed7e984efc6a5a57352
Robert Toth <kkdstryker@gmail.com>
2020-01-15 23:40:00 +0100

    Finish murder mystery example

--- bin/knowledge_base.ml
+++ bin/knowledge_base.ml
@@ -205,7 +205,7 @@ let _ = print_state_stream "domain" domain_state_stream
 
 
 let man_set = Base.Set.of_list (module Value.Comparator) [Value.str "george"; Value.str "john"; Value.str "robert"]
-let woman_set = Base.Set.of_list (module Value.Comparator) [Value.str "george"; Value.str "john"; Value.str "robert"]
+let woman_set = Base.Set.of_list (module Value.Comparator) [Value.str "barbara"; Value.str "christine"; Value.str "yolanda"]
 let person_set = Base.Set.union man_set woman_set
 let location_set = Base.Set.of_list (module Value.Comparator) [Value.str "bathroom"; Value.str "dining"; Value.str "kitchen"; Value.str "living room"; Value.str "pantry"; Value.str "study"]
 let weapon_set = Base.Set.of_list (module Value.Comparator) [Value.str "bag"; Value.str "firearm"; Value.str "gas"; Value.str "knife"; Value.str "poison"; Value.str "rope"]
@@ -237,10 +237,10 @@ let rope_var = Value.var "rope"
     let minus_a_b = Base.Set.diff person_set selected_set in
     Goal.in_set b (Value.set minus_a_b) state)*)
 
-let unique_people person_vars =
+let unique_people person_vars state =
   let first_person_var = Base.List.hd_exn person_vars in
   let rest_person_vars = Base.List.tl_exn person_vars in
-  let first_accum_goal = Goal.in_set first_person_var (Value.set person_set) State.empty in
+  let first_accum_goal = Goal.in_set first_person_var (Value.set person_set) state in
   let used_person_vars_set = Base.Set.singleton (module Value.Comparator) first_person_var in
   let (result, _) = Base.List.fold rest_person_vars ~init:(first_accum_goal, used_person_vars_set) ~f:(fun (accum_goal, used_person_vars_set) person_var -> 
     let next_used_person_vars_set = Base.Set.add used_person_vars_set person_var in
@@ -254,3 +254,54 @@ let unique_people person_vars =
   result
 
 (*TODO: implement the rest of the example*)
+
+let list_to_str_set list =
+  let mapped_list = Base.List.map list ~f:(fun element -> Value.str element) in
+  (Base.Set.of_list (module Value.Comparator) mapped_list)
+
+let list_to_var_set list =
+  let mapped_list = Base.List.map list ~f:(fun element -> Value.var element) in
+  (Base.Set.of_list (module Value.Comparator) mapped_list)
+
+let murderer x =
+  Goal.both_multi [
+    (fun state -> unique_people [bathroom_var; dining_var; kitchen_var; living_room_var; pantry_var; study_var] state);
+    (fun state -> unique_people [bag_var; firearm_var; gas_var; knife_var; poison_var; rope_var] state);
+    Goal.both_multi [Goal.in_set kitchen_var (Value.set man_set); Goal.in_set kitchen_var (Value.set(list_to_var_set ["gas"; "poison"]))];
+    Goal.both_multi [Goal.in_set study_var (Value.set (list_to_str_set ["barbara"; "yolanda"])); 
+      (fun state -> 
+        let study_val = State.value_of state study_var in
+        let set_without_study = Base.Set.remove (list_to_str_set ["barbara"; "yolanda"]) study_val |> Value.set in
+        Goal.in_set bathroom_var set_without_study state)];
+    Goal.both_multi [
+      (fun state -> 
+        let person_without_bg_set = Base.Set.diff person_set (list_to_str_set ["barbara"; "george"]) |> Value.set in
+        Goal.in_set bag_var person_without_bg_set state
+      );
+      (fun state ->
+        let variable_set = list_to_var_set ["kitchen"; "living room"; "pantry"; "study"] |> Value.set in
+        Goal.in_set bag_var variable_set state
+    )];
+    Goal.both_multi [Goal.in_set rope_var (woman_set |> Value.set); Goal.equal rope_var study_var];
+    (fun state -> 
+      let jg_set = list_to_str_set ["john"; "george"] |> Value.set in
+      Goal.in_set living_room_var jg_set state
+    );
+    (fun state -> 
+      let variable_set = list_to_var_set ["bathroom"; "kitchen"; "living room"; "pantry"; "study"] |> Value.set in
+      Goal.in_set knife_var variable_set state
+    );
+    (*(fun state ->
+      let variable_set = list_to_var_set ["bathroom"; "dining"; "kitchen"; "living room"] |> Value.set in
+      Goal.in_set (Value.str "yolanda") variable_set state
+    );*)
+    Goal.either_multi [Goal.equal bathroom_var (Value.str "yolanda"); Goal.equal dining_var (Value.str "yolanda"); Goal.equal kitchen_var (Value.str "yolanda"); Goal.equal living_room_var (Value.str "yolanda")];
+    Goal.equal firearm_var (Value.str "george");
+    Goal.both_multi [Goal.equal pantry_var gas_var; Goal.equal pantry_var x]
+  ]
+
+let x = Value.var "x"
+
+let murderer_stream = murderer x State.empty
+
+let _ = print_state_stream "murderer" murderer_stream
